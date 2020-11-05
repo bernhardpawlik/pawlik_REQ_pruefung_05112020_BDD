47715c783054170a2108966258a42eb7edf4b6fa
BP <wi19b024@technikum-wien.at>
2020-11-05 09:56:27 +0100

    Add project files.

---
+++ pawlik_REQ_pruefung_05112020/SpecFlowFeature1.feature
@@ -0,0 +1,20 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to calculate sun, cos and tan
+
+@mytag
+Scenario: sinus berechnen
+	Given the number is Pi
+	When sinus is called
+	Then the result should be 0
+
+Scenario: cosinus berechnen
+	Given the number is Pi
+	When cosinus is called
+	Then the result should be -1
+
+Scenario: tangens berechnen
+	Given the number is Pi
+	When tangens is called
+	Then the result should be 0
---
+++ pawlik_REQ_pruefung_05112020/SpecFlowFeature1Steps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+using Xunit.Sdk;
+
+namespace pawlik_REQ_pruefung_05112020
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        double v1;
+        double res;
+
+        [Given(@"the number is Pi")]
+        public void GivenTheNumberIsPi()
+        {
+            v1 = Math.PI;
+        }
+        
+        [When(@"sinus is called")]
+        public void WhenSinusIsCalled()
+        {
+            res = Math.Sin(v1);
+        }
+        
+        [When(@"cosinus is called")]
+        public void WhenCosinusIsCalled()
+        {
+            res = Math.Cos(v1);
+        }
+        
+        [When(@"tangens is called")]
+        public void WhenTangensIsCalled()
+        {
+            res = Math.Tan(v1);
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            Assert.Equal(p0, res, 5);
+        }
+    }
+}
